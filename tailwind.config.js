b8740d3a1248defa0b10b1179472ca3df4d3740e
Damian Meroni <meroni.damian@gmail.com>
2026-03-14 20:38:00 -0300

    feat: implement v5 neobrutalism design version

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-  darkMode: ['class', '[data-theme-state="dark"]'],
+  darkMode: ['class', '[data-theme="dark"]'],
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Manrope', 'system-ui', 'sans-serif'],
+        sans: ['Space Grotesk', 'system-ui', 'sans-serif'],
+        mono: ['JetBrains Mono', 'Courier New', 'monospace'],
+        display: ['Space Grotesk', 'Arial Black', 'sans-serif'],
       },
       typography: {
         DEFAULT: {
